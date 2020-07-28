9ea5219887b6eddde109f40c1e16d22e08933cb7
Olha Hrymchak <forwork1177@gmail.com>
2020-07-28 16:11:50 +0300

    added services

--- src/js/userData.js
+++ src/js/userData.js
@@ -4,28 +4,28 @@ export default {
     large_home_appliances: {
         name: "Крупная бытовая техника",
         value: "large_home_appliances",
-        image: '',
+        image: `big-tech.jpg`,
         categories: [],
         filter: ["refrigerators", "washing_machines", "dishwashers","сookers","freezers", "drying_machines" ]
     },
     built_in_appliances: {
         name: "Встраиваемая техника",
         value: "built_in_appliances",
-        image: '',
+        image: `build-in-tech.jpg`,
         categories: [],
         filter: ["built_in_ovens","built_in_hobs", "cooker_hoods","food_waste_disposers" , "Accessories_for_vbt" ]
     },
     home_and_clothing_care: {
         name: "Уход за домом и одеждой",
         value: "home_and_clothing_care",
-        image: '',
+        image: `home-care-tech.jpg`,
         categories: [],
         filter:["vacuum_cleaner","robot_vacuum_cleaners","irons","sewing_equipment_and_accessories", "steam_cleaners", "accessories_for_home_care_and_clothing_products"]
     },
     kitchen_appliances: {
         name: "Техника для кухни",
         value: "kitchen_appliances",
-        image: '',
+        image: `kitchen-tech.jpg`,
         categories: [],
         filter: ["coffee_machines","multicooker","microwave_ovens", "blenders","grills", "accessories_for_kitchen_appliances", "other_small_equipment"]
     }
