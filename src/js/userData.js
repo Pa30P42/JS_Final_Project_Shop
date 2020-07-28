67911cbb2d3b1e516bfba7d50fcb3c70c3845d86
Olha Hrymchak <forwork1177@gmail.com>
2020-07-28 15:49:14 +0300

    added services2

--- src/js/userData.js
+++ src/js/userData.js
@@ -6,33 +6,31 @@ export default {
         value: "large_home_appliances",
         image: '',
         categories: [],
-        filter: ["refrigerators", "washing_machines", "dishwashers", "сookers"]
+        filter: ["refrigerators", "washing_machines", "dishwashers","сookers","freezers", "drying_machines" ]
     },
     built_in_appliances: {
         name: "Встраиваемая техника",
         value: "built_in_appliances",
         image: '',
         categories: [],
-        filter: ["built_in_ovens", "built_in_hobs", "cooker_hoods", "Accessories_for_vbt", "food_waste_disposers"]
+        filter: ["built_in_ovens","built_in_hobs", "cooker_hoods","food_waste_disposers" , "Accessories_for_vbt" ]
     },
     home_and_clothing_care: {
         name: "Уход за домом и одеждой",
         value: "home_and_clothing_care",
         image: '',
         categories: [],
-        filter: ["refrigerators", "washing_machines", "dishwashers", "сookers"]
+        filter:["vacuum_cleaner","robot_vacuum_cleaners","irons","sewing_equipment_and_accessories", "steam_cleaners", "accessories_for_home_care_and_clothing_products"]
     },
     kitchen_appliances: {
-        name: "Техника для кухний",
+        name: "Техника для кухни",
         value: "kitchen_appliances",
         image: '',
         categories: [],
-        filter: ["refrigerators", "washing_machines", "dishwashers", "сookers"]
+        filter: ["coffee_machines","multicooker","microwave_ovens", "blenders","grills", "accessories_for_kitchen_appliances", "other_small_equipment"]
     }
 }
 }
-
-
 
 
 
