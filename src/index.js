85d2c2c6bea68f055a8b2e8c79016c7aaad2247a
Olha Hrymchak <forwork1177@gmail.com>
2020-07-28 13:54:45 +0300

    added services

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import './sass/main.scss';
-import { containerHandler } from './js/container/container';
-import './js/servicesTest/servicesTest';
+import {
+  containerHandler
+} from './js/container/container';
+import services from './js/services/services';
+import apiProducts from './js/api/products/apiProducts'
+import userData from './js/userData'
 
-containerHandler();
+apiProducts.getCategories().then(data => console.log(userData))
 
-console.log('hi');
+// services.addFavorite('5f154f156a4df46aa14dc526');
+// services.GetAllOrders();
---
+++ src/js/api/products/services.js
@@ -0,0 +1,20 @@
+import userData from '../../userData';
+
+export const getAppliances = (categories) => {
+  const getCategoryItems = (key) => {
+    const result = categories.reduce((acc, category) => {
+      for (const item of userData.appliances[key].filter) {
+        if (category.value === item) {
+          acc.push(category);
+          break;
+        }
+      }
+      return acc;
+    }, []);
+    userData.appliances[key].categories = [...result]
+  }
+  const keys = Object.keys(userData.appliances);
+  for (const key of keys) {
+    getCategoryItems(key)
+  }
+}
