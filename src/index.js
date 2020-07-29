b373eac0fc777430d0d057cbf04d44faf9f0369f
Ismayil <hajiev93@gmail.com>
2020-07-29 22:38:46 +0300

    before merging services2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './sass/main.scss';
-
 import './js/category/category-markup';
 import { containerHandler } from './js/container/container';
 import services from './js/services/services';
@@ -13,17 +12,10 @@ const btnRef = document.querySelector('.check');
 const btnRef2 = document.querySelector('.check2');
 
 containerHandler();
-// apiUsers.getInfo();
-btnRef.addEventListener('click', () => {
-  apiUsers.deleteFavorite('5f2155d59e8747001767cdf7');
-});
-btnRef2.addEventListener('click', () => {
-  apiUsers.getInfo();
-});
-// apiProducts.getCategories().then(data => console.log(userData));
 
-// services.addFavorite('5f154f156a4df46aa14dc526');
-// services.GetAllOrders();
-
-// btnRef.addEventListener('click', apiAuth.login);
-// btnRef2.addEventListener('click', getInfo);
+// btnRef.addEventListener('click', () => {
+//   apiUsers.deleteFavorite('5f2155d59e8747001767cdf7');
+// });
+// btnRef2.addEventListener('click', () => {
+//   apiUsers.getInfo();
+// });
