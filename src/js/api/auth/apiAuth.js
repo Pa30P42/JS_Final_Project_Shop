7b26e7955578f9cba08d346a6c8a7ae3c3664a74
Ismayil <hajiev93@gmail.com>
2020-07-29 16:29:59 +0300

    start of object

--- src/js/api/auth/apiAuth.js
+++ src/js/api/auth/apiAuth.js
@@ -18,17 +18,20 @@ const deleteFavoriteUrl =
 const btnRef = document.querySelector('.check');
 const btnRef2 = document.querySelector('.check2');
 
-const addFavorite = async () => {
-  try {
-    axios.defaults.headers['Authorization'] =
-      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMWZlMDk0ZjlkMWZiMDAxNzQ0MGYyMiIsImlhdCI6MTU5NTk1MTIxNywiZXhwIjoxNTk2MDM3NjE3fQ.fkNST8eulZySCYzg-vVOYBs-Ircia1yN-lHY8M82daE';
-    const response = await axios.get(addFavoriteUrl);
-    console.log(response);
-  } catch (error) {
-    console.log(error);
-    // throw error;
-  }
+export default {
+  addFavorite: async () => {
+    try {
+      axios.defaults.headers['Authorization'] =
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMWZlMDk0ZjlkMWZiMDAxNzQ0MGYyMiIsImlhdCI6MTU5NTk1MTIxNywiZXhwIjoxNTk2MDM3NjE3fQ.fkNST8eulZySCYzg-vVOYBs-Ircia1yN-lHY8M82daE';
+      const response = await axios.get(addFavoriteUrl);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+      // throw error;
+    }
+  },
 };
+
 const deleteFavorite = async () => {
   try {
     axios.defaults.headers['Authorization'] =
@@ -95,10 +98,9 @@ const changeUserPassword = async () => {
     // throw error;
   }
 };
-const getCurrentUser = async () => {
+const getCurrentUser = async token => {
   try {
-    axios.defaults.headers['Authorization'] =
-      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMWZlMDk0ZjlkMWZiMDAxNzQ0MGYyMiIsImlhdCI6MTU5NTk1MTIxNywiZXhwIjoxNTk2MDM3NjE3fQ.fkNST8eulZySCYzg-vVOYBs-Ircia1yN-lHY8M82daE';
+    axios.defaults.headers['Authorization'] = token;
     const response = await axios.get(getUserInfoUrl);
     console.log(response);
   } catch (error) {
@@ -166,9 +168,19 @@ const login = async () => {
       password: 'testuno111',
     };
     const response = await axios.post(loginUrl, user);
-    localStorage.setItem('token', response.data.accces_token);
+    axios.defaults.headers['Authorization'] = response.data.accces_token;
+    const currentUser = await axios.get(getUserInfoUrl);
+
+    // console.log(currentUser.data.favorites);
+    localStorage.setItem(
+      'info',
+      JSON.stringify({
+        token: response.data.accces_token,
+        favorites: [...currentUser.data.favorites],
+      }),
+    );
     // console.log(response);
-    console.log(response.data.accces_token);
+    // console.log(response);
     // console.log(response);
   } catch (error) {
     console.log(error);
@@ -177,4 +189,174 @@ const login = async () => {
 };
 
 btnRef.addEventListener('click', login);
-btnRef2.addEventListener('click', getCurrentUser);
+btnRef2.addEventListener('click', addFavorite);
+
+// const addFavorite = async () => {
+//   try {
+//     axios.defaults.headers['Authorization'] =
+//       'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMWZlMDk0ZjlkMWZiMDAxNzQ0MGYyMiIsImlhdCI6MTU5NTk1MTIxNywiZXhwIjoxNTk2MDM3NjE3fQ.fkNST8eulZySCYzg-vVOYBs-Ircia1yN-lHY8M82daE';
+//     const response = await axios.get(addFavoriteUrl);
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//     // throw error;
+//   }
+// };
+// const deleteFavorite = async () => {
+//   try {
+//     axios.defaults.headers['Authorization'] =
+//       'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMWZlMDk0ZjlkMWZiMDAxNzQ0MGYyMiIsImlhdCI6MTU5NTk1MTIxNywiZXhwIjoxNTk2MDM3NjE3fQ.fkNST8eulZySCYzg-vVOYBs-Ircia1yN-lHY8M82daE';
+//     const response = await axios.delete(deleteFavoriteUrl);
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//     // throw error;
+//   }
+// };
+
+// const getInfo = async () => {
+//   try {
+//     const response = await axios.get(usersUrl);
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//     // throw error;
+//   }
+// };
+// const changeUserInfo = async () => {
+//   try {
+//     axios.defaults.headers['Authorization'] =
+//       'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMWZlMDk0ZjlkMWZiMDAxNzQ0MGYyMiIsImlhdCI6MTU5NTk1MTIxNywiZXhwIjoxNTk2MDM3NjE3fQ.fkNST8eulZySCYzg-vVOYBs-Ircia1yN-lHY8M82daE';
+//     const user = { email: 'cheeseburger555555@gmail.com' };
+//     const response = await axios.patch(usersUrl, user);
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//     // throw error;
+//   }
+// };
+// const updateUserAddress = async () => {
+//   try {
+//     axios.defaults.headers['Authorization'] =
+//       'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMWZlMDk0ZjlkMWZiMDAxNzQ0MGYyMiIsImlhdCI6MTU5NTk1MTIxNywiZXhwIjoxNTk2MDM3NjE3fQ.fkNST8eulZySCYzg-vVOYBs-Ircia1yN-lHY8M82daE';
+//     const user = {
+//       country: 'USA',
+//       city: 'NY',
+//       place: 'Brroklyn',
+//       street: 'Wall street',
+//       block: '1',
+//       building: '',
+//       flat: '15',
+//       zip: '',
+//     };
+//     const response = await axios.patch(updateAddressUrl, user);
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//     // throw error;
+//   }
+// };
+// const changeUserPassword = async () => {
+//   try {
+//     axios.defaults.headers['Authorization'] =
+//       'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMWZlMDk0ZjlkMWZiMDAxNzQ0MGYyMiIsImlhdCI6MTU5NTk1MTIxNywiZXhwIjoxNTk2MDM3NjE3fQ.fkNST8eulZySCYzg-vVOYBs-Ircia1yN-lHY8M82daE';
+//     const newPassword = { password: 'qwerty123', confirmPassword: 'qwerty123' };
+//     const response = await axios.patch(changePasswordUrl, newPassword);
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//     // throw error;
+//   }
+// };
+// const getCurrentUser = async token => {
+//   try {
+//     axios.defaults.headers['Authorization'] = token;
+//     const response = await axios.get(getUserInfoUrl);
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//     // throw error;
+//   }
+// };
+// const getUserInformation = () => {
+//   const getUserById = async id => {
+//     try {
+//       axios.defaults.headers['Authorization'] =
+//         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMWZlMDk0ZjlkMWZiMDAxNzQ0MGYyMiIsImlhdCI6MTU5NTk1MTIxNywiZXhwIjoxNTk2MDM3NjE3fQ.fkNST8eulZySCYzg-vVOYBs-Ircia1yN-lHY8M82daE';
+//       const response = await axios.get(`${getUserByIdUrl}${id}`);
+//       console.log(response);
+//     } catch (error) {
+//       console.log(error);
+//       // throw error;
+//     }
+//   };
+//   const getUserInfo = async () => {
+//     try {
+//       axios.defaults.headers['Authorization'] =
+//         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMWZlMDk0ZjlkMWZiMDAxNzQ0MGYyMiIsImlhdCI6MTU5NTk1MTIxNywiZXhwIjoxNTk2MDM3NjE3fQ.fkNST8eulZySCYzg-vVOYBs-Ircia1yN-lHY8M82daE';
+//       const response = await axios.get(getUserInfoUrl);
+//       const currentId = response.data._id;
+//       console.log(currentId);
+//       return currentId;
+//     } catch (error) {
+//       console.log(error);
+//       // throw error;
+//     }
+//   };
+//   getUserInfo().then(id => getUserById(id));
+// };
+// const getUserById = async id => {
+//   try {
+//     axios.defaults.headers['Authorization'] =
+//       'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMWZlMDk0ZjlkMWZiMDAxNzQ0MGYyMiIsImlhdCI6MTU5NTk1MTIxNywiZXhwIjoxNTk2MDM3NjE3fQ.fkNST8eulZySCYzg-vVOYBs-Ircia1yN-lHY8M82daE';
+//     const response = await axios.get(`${getUserByIdUrl}${id}`);
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//     // throw error;
+//   }
+// };
+
+// const register = async () => {
+//   try {
+//     const user = {
+//       email: 'ismayilnew@gmail.com',
+//       password: 'ismayil123123',
+//     };
+//     const response = await axios.post(regUrl, user);
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//     // throw error;
+//   }
+// };
+
+// const login = async () => {
+//   try {
+//     const user = {
+//       email: 'unotest2@gmail.com',
+//       password: 'testuno111',
+//     };
+//     const response = await axios.post(loginUrl, user);
+//     axios.defaults.headers['Authorization'] = response.data.accces_token;
+//     const currentUser = await axios.get(getUserInfoUrl);
+
+//     // console.log(currentUser.data.favorites);
+//     localStorage.setItem(
+//       'info',
+//       JSON.stringify({
+//         token: response.data.accces_token,
+//         favorites: [...currentUser.data.favorites],
+//       }),
+//     );
+//     // console.log(response);
+//     // console.log(response);
+//     // console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//     throw new Error(error);
+//   }
+// };
+
+// btnRef.addEventListener('click', login);
+// btnRef2.addEventListener('click', getInfo);
