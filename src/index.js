54bada7a5487e21e71eb409fb8f83b6f10a13cb6
nyuta <pillowforneedles@gmail.com>
2020-07-29 22:10:58 +0300

    init adv

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './sass/main.scss';
+import './js/adv/index';
+import updateLastSeen from './js/adv/index';
+import markupProductCard from './js/adv/';
+
+// document.body.onload = updateLastSeen(189864298, new Date().getTime());
---
+++ src/js/adv/lastSeen.js
@@ -0,0 +1,21 @@
+const LAST_SEEN = 'lastSeen';
+
+function updateLastSeen(id, time) {
+  const lastSeenData = localStorage.getItem(lastSeen); //массив с просмотренными элементами
+  let lastSeen = JSON.parse(lastSeenData);
+  if (!lastSeen) {
+    lastSeen = [];
+  }
+  const item = lastSeen.find(elem => elem.id === id);
+  if (!item) {
+    lastSeen.push({
+      id,
+      time,
+    });
+  } else {
+    lastSeen[lastSeen.indexOf(item)].time = time;
+  }
+  localStorage.setItem(LAST_SEEN, JSON.stringify(lastSeen));
+}
+
+export default updateLastSeen;
