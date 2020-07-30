a19422a8824e4a8b0df21a7d84a35a9bca8a88c0
nyuta <pillowforneedles@gmail.com>
2020-07-30 15:35:40 +0300

    init adv

--- src/index.js
+++ src/index.js
@@ -11,16 +11,17 @@ import setting from './js/setting';
 
 // =========== adv ==============
 import './js/adv/index';
-import updateLastSeen from './js/adv/index';
-import markupProductCard from './js/adv/';
+import updateLastSeen from './js/adv/lastSeen';
 
-// document.body.onload = updateLastSeen(189864298, new Date().getTime());
+document.body.onload = updateLastSeen(189864298, new Date().getTime());
 // =============================
 
 const btnRef = document.querySelector('.check');
 const btnRef2 = document.querySelector('.check2');
 
 containerHandler();
+
+btnRef.addEventListener('click', markupProductCard);
 
 // btnRef.addEventListener('click', () => {
 //   apiUsers.deleteFavorite('5f2155d59e8747001767cdf7');
--- src/js/adv/markup.js
+++ src/js/adv/markup.js
@@ -1,31 +1,45 @@
-export const saleCartItem = element => {
-  return `<li class="sale_item" data-id=${element._id}>
-  <div class="sale-image">
-    <img class="sale_img" src="./images/sale/image1.png" alt="img" width="86"/>
-    <img class="sale_img-tablet" src="./images/sale/image1_tb.png" alt="img" width="149"/>
-  </div>
-  <img class="sale_vector" src="./images/sale/Vector.svg" />
-  <p class="sale_name">${element.name}</p>
-  <ul class="sale_rating-list">${rating()}</ul>
-  <div class="sale_prise-block">
-    <p class="sale_price">${element.price}<span> &#8372;</span></p>
-  </div>
-</li>`;
-};
-const createListMarkup = array => {
-  return `<ul class="sale_list">${array.reduce((acc, element) => {
-    acc += saleCartItem(element);
-    return acc;
-  }, '')}</ul>`;
-};
-const getItem = event => {
-  if (event.target !== current.target) {
-    const id = event.target.closest('[data-id]').dataset.id;
-    // функция Ани(id)
-  } else return id;
-};
-const createList = array => {
-  const container = document.querySelector('.container');
-  container.innerHTML = createListMarkup(array);
-  container.addEventListener('click', getItem);
-};
+import '../../sass/main.scss';
+import { refs } from '../components/refs';
+export function markupProduct() {
+  let markup = `
+  
+  
+  `;
+
+  refs.sections.innerHTML = markup;
+  return markup;
+}
+
+export default markupProduct;
+
+// export const saleCartItem = element => {
+//   return `<li class="sale_item" data-id=${element._id}>
+//   <div class="sale-image">
+//     <img class="sale_img" src="./images/sale/image1.png" alt="img" width="86"/>
+//     <img class="sale_img-tablet" src="./images/sale/image1_tb.png" alt="img" width="149"/>
+//   </div>
+//   <img class="sale_vector" src="./images/sale/Vector.svg" />
+//   <p class="sale_name">${element.name}</p>
+//   <ul class="sale_rating-list">${rating()}</ul>
+//   <div class="sale_prise-block">
+//     <p class="sale_price">${element.price}<span> &#8372;</span></p>
+//   </div>
+// </li>`;
+// };
+// const createListMarkup = array => {
+//   return `<ul class="sale_list">${array.reduce((acc, element) => {
+//     acc += saleCartItem(element);
+//     return acc;
+//   }, '')}</ul>`;
+// };
+// const getItem = event => {
+//   if (event.target !== current.target) {
+//     const id = event.target.closest('[data-id]').dataset.id;
+//     // функция Ани(id)
+//   } else return id;
+// };
+// const createList = array => {
+//   const container = document.querySelector('.container');
+//   container.innerHTML = createListMarkup(array);
+//   container.addEventListener('click', getItem);
+// };
