4cb48c7c2d4198e99e90449080e648228b168e7e
OlegKhodakivskyi <oleghodakovsky@gmail.com>
2020-07-30 13:46:59 +0300

    final Modal Module

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import './sass/main.scss';
 import './js/api/auth/apiAuth';
-import { containerHandler } from './js/container/container';
 
-import userData from './js/userData';
 import apiAuth from './js/api/auth/apiAuth';
 import apiUsers from './js/api/users/apiUsers';
 import './js/category/category-markup';
 import { containerHandler } from './js/container/container';
 import './js/components/modalModule/modalModule';
-import services from './js/services/services';
 import apiProducts from './js/api/products/apiProducts';
 import userData from './js/userData';
 
