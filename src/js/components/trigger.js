13c417739b1fc71f7f1f82ebf1d9724955369d6f
Ismayil <hajiev93@gmail.com>
2020-07-30 11:57:48 +0300

    11;57 trigger

--- src/js/components/trigger.js
+++ src/js/components/trigger.js
@@ -1,15 +1,10 @@
 import { refs } from './refs';
 
 export default {
-  openTrigger() {
-    console.log(refs.triggerHidden);
-    if (refs.triggerHidden.classList.contains('ishiddenTrigger')) {
-      refs.triggerHidden.classList.remove('ishiddenTrigger');
-    }
-  },
+  //   triggerHidden: document.querySelector('.ishiddenTrigger'),
   triggerMarkup() {
     return `
-          <div class="trigger-wrapper ishiddenTrigger">
+          <div class="trigger-wrapper ">
              <ul class="trigger-list">
                <li class="trigger-list__item"><a href="tel:380503333796" class="trigger-list__item_vodafone">+38 (050)
                    333-37-96</a></li>
@@ -21,9 +16,17 @@ export default {
             </div>
         `;
   },
-
-  triggerFn() {
-    refs.sections.insertAdjacentHTML('afterbegin', this.triggerMarkup());
+  openTrigger() {
+    console.log(this.triggerHidden);
+    if (refs.triggerHidden.classList.contains('ishiddenTrigger')) {
+      refs.triggerHidden.classList.remove('ishiddenTrigger');
+    }
+  },
+  async triggerFn() {
+    await refs.sections.insertAdjacentHTML('afterbegin', this.triggerMarkup());
+    const triigerWrapper = await document.querySelector('trigger-wrapper');
+    const triggerHidden = document.querySelector('ishiddenTrigger');
+    console.log(triigerWrapper);
   },
 };
 // triggerFn();
