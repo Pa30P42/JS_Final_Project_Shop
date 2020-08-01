acc974811c5b3309af04c87610297a93f61b4da3
Anna Horianina <anna.goryanina88@gmail.com>
2020-08-01 21:47:12 +0300

    added slider 21:46

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import './sass/main.scss';
 
 import './js/category/category-markup';
 import { containerHandler } from './js/container/container';
-import services from './js/services/services';
-import apiProducts from './js/api/products/apiProducts';
-import userData from './js/userData';
-import './js/slider/slider.js';
-import { Sim } from './js/slider/slider';
-containerHandler();
-apiProducts.getCategories().then(data => console.log(userData));
+import './js/slider/slider';
+// import services from './js/services/services';
+// import apiProducts from './js/api/products/apiProducts';
+// import userData from './js/userData';
+// import './js/slider/slider.js';
+// import { Sim } from './js/slider/slider';
+// containerHandler();
+
+// apiProducts.getCategories().then(data => console.log(userData));
 
 // services.addFavorite('5f154f156a4df46aa14dc526');
 // services.GetAllOrders();
+// sliderItemMarkup();
--- src/js/slider/slider.js
+++ src/js/slider/slider.js
@@ -1,66 +1,11 @@
-import screen from '../../images/slider/slider_mb/new/img2.jpg';
+import { sliderItemMarkup } from './sliderMarkup';
+import { refs } from '../components/refs';
 // ./images/slider/slider_mb/new/img2.jpg
-
 const sliderRef = document.querySelector('.slider-wrapper');
+console.log(refs.sections);
+refs.sections.innerHTML = sliderItemMarkup();
 
 export function Sim(sldrId) {
-  const sliderItemMarkup = () => {
-    return `<div class="sim-slider">
-  <ul class="sim-slider-list">
-    <li><img
-    src="${screen}"
-    alt="screen"
-    class="sim-slider-list-image"
-  />
-</li>
-<img
-              src="${screen}"
-              alt="0"
-              class="sim-slider-list-image"
-            />
-          </li>
-          <li class="sim-slider-element">
-<li class="sim-slider-element">
-<img
-              src="./images/slider/slider_mb/new/img1.jpg"
-              alt="1"
-              class="sim-slider-list-image"
-            />
-          </li>
-          <li class="sim-slider-element">
-          <img
-              src="./images/slider/slider_mb/new/img4.jpg"
-              alt="2"
-              class="sim-slider-list-image"
-            />
-          </li>
-          <li class="sim-slider-element">
-          <img
-              src="./images/slider/slider_mb/new/img3.jpg"
-              alt="3"
-              class="sim-slider-list-image"
-            />
-          </li>
-          </ul>
-
-        <div class="sim-slider-arrow-left">
-          <img
-            class="sim-slider-arrow-left-img"
-            src="./images/slider/slider_mb/btn_left.png"
-          />
-        </div>
-        <div class="sim-slider-arrow-right">
-          <img
-            class="sim-slider-arrow-right-img"
-            src="./images/slider/slider_mb/btn_right.png"
-          />
-        </div>
-
-        <div class="sim-slider-dots"></div>
-      </div>`;
-  };
-  sliderRef.innerHTML = sliderItemMarkup();
-
   let id = document.getElementById(sldrId);
   if (id) {
     this.sldrRoot = id;
@@ -79,188 +24,188 @@ export function Sim(sldrId) {
   // Initialization
   this.options = Sim.defaults;
   Sim.initialize(this);
+}
+// const sliderRef = document.querySelector('.slider-wrapper');
 
-  // const sliderRef = document.querySelector('.slider-wrapper');
+Sim.defaults = {
+  // Default options for the carousel
+  loop: true, // Бесконечное зацикливание слайдера
+  auto: true, // Автоматическое пролистывание
+  interval: 5000, // Интервал между пролистыванием элементов (мс)
+  arrows: true, // Пролистывание стрелками
+  dots: true, // Индикаторные точки
+};
 
-  Sim.defaults = {
-    // Default options for the carousel
-    loop: true, // Бесконечное зацикливание слайдера
-    auto: true, // Автоматическое пролистывание
-    interval: 5000, // Интервал между пролистыванием элементов (мс)
-    arrows: true, // Пролистывание стрелками
-    dots: true, // Индикаторные точки
-  };
+Sim.prototype.elemPrev = function (num) {
+  num = num || 1;
 
-  Sim.prototype.elemPrev = function (num) {
-    num = num || 1;
+  let prevElement = this.currentElement;
+  this.currentElement -= num;
+  if (this.currentElement < 0) this.currentElement = this.elemCount - 1;
 
-    let prevElement = this.currentElement;
-    this.currentElement -= num;
-    if (this.currentElement < 0) this.currentElement = this.elemCount - 1;
+  if (!this.options.loop) {
+    if (this.currentElement == 0) {
+      this.leftArrow.style.display = 'none';
+    }
+    this.rightArrow.style.display = 'block';
+  }
 
-    if (!this.options.loop) {
-      if (this.currentElement == 0) {
-        this.leftArrow.style.display = 'none';
+  this.sldrElements[this.currentElement].style.opacity = '1';
+  this.sldrElements[prevElement].style.opacity = '0';
+
+  if (this.options.dots) {
+    this.dotOn(prevElement);
+    this.dotOff(this.currentElement);
+  }
+};
+
+Sim.prototype.elemNext = function (num) {
+  num = num || 1;
+
+  let prevElement = this.currentElement;
+  this.currentElement += num;
+  if (this.currentElement >= this.elemCount) this.currentElement = 0;
+
+  if (!this.options.loop) {
+    if (this.currentElement == this.elemCount - 1) {
+      this.rightArrow.style.display = 'none';
+    }
+    this.leftArrow.style.display = 'block';
+  }
+
+  this.sldrElements[this.currentElement].style.opacity = '1';
+  this.sldrElements[prevElement].style.opacity = '0';
+
+  if (this.options.dots) {
+    this.dotOn(prevElement);
+    this.dotOff(this.currentElement);
+  }
+};
+
+Sim.prototype.dotOn = function (num) {
+  this.indicatorDotsAll[num].style.cssText =
+    'background-color:#B3B3B3; cursor:pointer;';
+};
+
+Sim.prototype.dotOff = function (num) {
+  this.indicatorDotsAll[num].style.cssText =
+    'background-image:linear-gradient(11.4deg, #6368e5 15.48%, #b884f3 81.25%); cursor:default;';
+};
+
+Sim.initialize = function (that) {
+  // Constants
+  that.elemCount = that.sldrElements.length; // Количество элементов
+
+  // Variables
+  that.currentElement = 0;
+  let bgTime = getTime();
+
+  // Functions
+  function getTime() {
+    return new Date().getTime();
+  }
+  function setAutoScroll() {
+    that.autoScroll = setInterval(function () {
+      let fnTime = getTime();
+      if (fnTime - bgTime + 10 > that.options.interval) {
+        bgTime = fnTime;
+        that.elemNext();
       }
-      this.rightArrow.style.display = 'block';
-    }
+    }, that.options.interval);
+  }
 
-    this.sldrElements[this.currentElement].style.opacity = '1';
-    this.sldrElements[prevElement].style.opacity = '0';
+  // Start initialization
+  if (that.elemCount <= 1) {
+    // Отключить навигацию
+    that.options.auto = false;
+    that.options.arrows = false;
+    that.options.dots = false;
+    that.leftArrow.style.display = 'none';
+    that.rightArrow.style.display = 'none';
+  }
+  if (that.elemCount >= 1) {
+    // показать первый элемент
+    that.sldrElemFirst.style.opacity = '1';
+  }
 
-    if (this.options.dots) {
-      this.dotOn(prevElement);
-      this.dotOff(this.currentElement);
-    }
-  };
+  if (!that.options.loop) {
+    that.leftArrow.style.display = 'none'; // отключить левую стрелку
+    that.options.auto = false; // отключить автопркрутку
+  } else if (that.options.auto) {
+    // инициализация автопрокруки
+    setAutoScroll();
+    // Остановка прокрутки при наведении мыши на элемент
+    that.sldrList.addEventListener(
+      'mouseenter',
+      function () {
+        clearInterval(that.autoScroll);
+      },
+      false,
+    );
+    that.sldrList.addEventListener('mouseleave', setAutoScroll, false);
+  }
 
-  Sim.prototype.elemNext = function (num) {
-    num = num || 1;
-
-    let prevElement = this.currentElement;
-    this.currentElement += num;
-    if (this.currentElement >= this.elemCount) this.currentElement = 0;
-
-    if (!this.options.loop) {
-      if (this.currentElement == this.elemCount - 1) {
-        this.rightArrow.style.display = 'none';
-      }
-      this.leftArrow.style.display = 'block';
-    }
-
-    this.sldrElements[this.currentElement].style.opacity = '1';
-    this.sldrElements[prevElement].style.opacity = '0';
-
-    if (this.options.dots) {
-      this.dotOn(prevElement);
-      this.dotOff(this.currentElement);
-    }
-  };
-
-  Sim.prototype.dotOn = function (num) {
-    this.indicatorDotsAll[num].style.cssText =
-      'background-color:#B3B3B3; cursor:pointer;';
-  };
-
-  Sim.prototype.dotOff = function (num) {
-    this.indicatorDotsAll[num].style.cssText =
-      'background-image:linear-gradient(11.4deg, #6368e5 15.48%, #b884f3 81.25%); cursor:default;';
-  };
-
-  Sim.initialize = function (that) {
-    // Constants
-    that.elemCount = that.sldrElements.length; // Количество элементов
-
-    // Variables
-    that.currentElement = 0;
-    let bgTime = getTime();
-
-    // Functions
-    function getTime() {
-      return new Date().getTime();
-    }
-    function setAutoScroll() {
-      that.autoScroll = setInterval(function () {
+  if (that.options.arrows) {
+    // инициализация стрелок
+    that.leftArrow.addEventListener(
+      'click',
+      function () {
         let fnTime = getTime();
-        if (fnTime - bgTime + 10 > that.options.interval) {
+        if (fnTime - bgTime > 1000) {
+          bgTime = fnTime;
+          that.elemPrev();
+        }
+      },
+      false,
+    );
+    that.rightArrow.addEventListener(
+      'click',
+      function () {
+        let fnTime = getTime();
+        if (fnTime - bgTime > 1000) {
           bgTime = fnTime;
           that.elemNext();
         }
-      }, that.options.interval);
-    }
+      },
+      false,
+    );
+  } else {
+    that.leftArrow.style.display = 'none';
+    that.rightArrow.style.display = 'none';
+  }
 
-    // Start initialization
-    if (that.elemCount <= 1) {
-      // Отключить навигацию
-      that.options.auto = false;
-      that.options.arrows = false;
-      that.options.dots = false;
-      that.leftArrow.style.display = 'none';
-      that.rightArrow.style.display = 'none';
+  if (that.options.dots) {
+    // инициализация индикаторных точек
+    let sum = '',
+      diffNum;
+    for (let i = 0; i < that.elemCount; i++) {
+      sum += '<span class="sim-dot" ></span>';
     }
-    if (that.elemCount >= 1) {
-      // показать первый элемент
-      that.sldrElemFirst.style.opacity = '1';
-    }
-
-    if (!that.options.loop) {
-      that.leftArrow.style.display = 'none'; // отключить левую стрелку
-      that.options.auto = false; // отключить автопркрутку
-    } else if (that.options.auto) {
-      // инициализация автопрокруки
-      setAutoScroll();
-      // Остановка прокрутки при наведении мыши на элемент
-      that.sldrList.addEventListener(
-        'mouseenter',
-        function () {
-          clearInterval(that.autoScroll);
-        },
-        false,
-      );
-      that.sldrList.addEventListener('mouseleave', setAutoScroll, false);
-    }
-
-    if (that.options.arrows) {
-      // инициализация стрелок
-      that.leftArrow.addEventListener(
+    that.indicatorDots.innerHTML = sum;
+    that.indicatorDotsAll = that.sldrRoot.querySelectorAll('span.sim-dot');
+    // Назначаем точкам обработчик события 'click'
+    for (let n = 0; n < that.elemCount; n++) {
+      that.indicatorDotsAll[n].addEventListener(
         'click',
         function () {
-          let fnTime = getTime();
-          if (fnTime - bgTime > 1000) {
-            bgTime = fnTime;
-            that.elemPrev();
+          diffNum = Math.abs(n - that.currentElement);
+          if (n < that.currentElement) {
+            bgTime = getTime();
+            that.elemPrev(diffNum);
+          } else if (n > that.currentElement) {
+            bgTime = getTime();
+            that.elemNext(diffNum);
           }
+          // Если n == that.currentElement ничего не делаем
         },
         false,
       );
-      that.rightArrow.addEventListener(
-        'click',
-        function () {
-          let fnTime = getTime();
-          if (fnTime - bgTime > 1000) {
-            bgTime = fnTime;
-            that.elemNext();
-          }
-        },
-        false,
-      );
-    } else {
-      that.leftArrow.style.display = 'none';
-      that.rightArrow.style.display = 'none';
     }
+    that.dotOff(0); // точка[0] выключена, остальные включены
+    for (let i = 1; i < that.elemCount; i++) {
+      that.dotOn(i);
+    }
+  }
+};
 
-    if (that.options.dots) {
-      // инициализация индикаторных точек
-      let sum = '',
-        diffNum;
-      for (let i = 0; i < that.elemCount; i++) {
-        sum += '<span class="sim-dot" ></span>';
-      }
-      that.indicatorDots.innerHTML = sum;
-      that.indicatorDotsAll = that.sldrRoot.querySelectorAll('span.sim-dot');
-      // Назначаем точкам обработчик события 'click'
-      for (let n = 0; n < that.elemCount; n++) {
-        that.indicatorDotsAll[n].addEventListener(
-          'click',
-          function () {
-            diffNum = Math.abs(n - that.currentElement);
-            if (n < that.currentElement) {
-              bgTime = getTime();
-              that.elemPrev(diffNum);
-            } else if (n > that.currentElement) {
-              bgTime = getTime();
-              that.elemNext(diffNum);
-            }
-            // Если n == that.currentElement ничего не делаем
-          },
-          false,
-        );
-      }
-      that.dotOff(0); // точка[0] выключена, остальные включены
-      for (let i = 1; i < that.elemCount; i++) {
-        that.dotOn(i);
-      }
-    }
-  };
-}
 new Sim();
