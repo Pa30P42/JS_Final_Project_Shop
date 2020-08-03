c9c36edb03f686ac54e2fcfa148ff2040d810576
OlegKhodakivskyi <oleghodakovsky@gmail.com>
2020-08-03 18:02:50 +0300

    authMenu link alsmo done

--- src/js/auth/authMenu.js
+++ src/js/auth/authMenu.js
@@ -1,34 +1,50 @@
 import { modalModule } from '../components/modalModule/modalModule';
 import { authForm } from '../auth/authForm';
 import apiAuth from '../api/auth/apiAuth';
+import apiUsers from '../api/users/apiUsers';
+
 let authFormListeners = '';
 const userValue = {
   email: '',
   password: '',
 };
 
-// const userRegisterValue = {
-//   email: '',
-//   password: '',
-// };
+
+const modalContainer = document.querySelector('.modalModule');
+let loginAccount = '';
+
+const privateMenu = function (e) {
+  const dataway = e.target.classList;
+  if (dataway.contains('privateAccount')) {
+  }
+  if (dataway.contains('favoritesAccount')) {
+  }
+  if (dataway.contains('createAdAccount')) {
+  }
+  if (dataway.contains('exitAccount')) {
+  }
+
+  console.dir(dataway);
+};
 
 export const authFn = function () {
   if (localStorage.getItem('info')) {
     modalModule(authMenuMarkUp, authMenuMarkUpListener);
+    loginAccount = document.querySelector('.auth-menu');
+    loginAccount.addEventListener('click', privateMenu);
   } else {
     modalModule(authForm, authMenuMarkUpListener);
     authFormListeners = document.querySelector('.authForm');
     authFormListeners.addEventListener('click', e => {
       e.preventDefault();
       if (e.target === e.currentTarget[2]) {
-        console.log('logging');
         userValue.email = e.currentTarget[0].value;
         userValue.password = e.currentTarget[1].value;
         apiAuth.login(userValue);
         e.currentTarget.reset();
+        modalContainer.innerHTML = '';
       }
       if (e.target === e.currentTarget[3]) {
-        console.log('register');
         userValue.email = e.currentTarget[0].value;
         userValue.password = e.currentTarget[1].value;
         apiAuth.register(userValue);
@@ -38,19 +54,26 @@ export const authFn = function () {
   }
 };
 
-// if (e.target === e.currentTarget[3])
+// const userName = apiUsers.getCurrentUser().then(response => {
+//   return response.data.name;
+// });
 
 let listenPrivateAccount = '';
 const authMenuMarkUp = function () {
   return `
     <div class="auth-menu">
-    <h4 class="user-name">Username</h4>
+    <h4 class="user-name">Username: ${apiUsers
+      .getCurrentUser()
+      .then(response => {
+        return response.data.name;
+      })}
+</h4 >
     <ul class="auth-menu__list">
-      <li class="auth-menu__list_item privateAccount">Личный кабинет</li>
-      <li class="auth-menu__list_item favorites">Избранное</li>
-      <li class="auth-menu__list_item createAd">Создать объявление</li>
+      <li class="auth-menu__list_item privateAccount" date-way="privateaccount"> Личный кабинет</li>
+      <li class="auth-menu__list_item favoritesAccount" date-way="privatefavorites"> Избранное</li>
+      <li class="auth-menu__list_item createAdAccount" date-way="createad"> Создать объявление</li>
     </ul>
-    <p class="auth-menu__exit" class="exit">Выход</p>
+    <p class="auth-menu__exit exitAccount" date-way="exit"> Выход</p>
   </div>
   `;
 };
@@ -58,7 +81,3 @@ const authMenuMarkUp = function () {
 const authMenuMarkUpListener = function () {
   listenPrivateAccount = document.querySelector('.auth-menu__list');
 };
-
-// authMenuMarkUpFn() {
-//     modalModule(this.authMenuMarkUp, this.authMenuMarkUpListener)
-// }
