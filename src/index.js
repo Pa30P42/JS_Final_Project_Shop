bade802844ec2efcb3fa6876f0b16335703bbcbf
Anastasia-Dmv <Vesta50000@gmail.com>
2020-08-04 20:18:33 +0300

    profile work with apiAuth only

--- src/index.js
+++ src/index.js
@@ -87,7 +87,83 @@
 
 // // };
 // // initialActProfile();
+//=============================
+// import './sass/main.scss';
+// import './js/category/category-markup';
+// import {
+//   containerHandler
+// } from './js/container/container';
+// import {
+//   createSingleCardMarkup
+// } from './js/sale/cardModule';
+// import {
+//   createList
+// } from './js/sale/saleSection';
+// import {
+//   Sim
+// } from './js/slider/slider';
+// import './js/catalog/catalog';
+// import './js/components/modalModule/modalModule';
+// import apiProducts from './js/api/products/apiProducts';
+// import apiOrders from './js/api/orders/apiOrders';
+// import setting from './js/setting';
+// import {
+//   refs
+// } from './js/components/refs';
+// import './js/category/category-markup';
+// import axios from 'axios';
+// import apiAuth from './js/api/auth/apiAuth';
+// import apiUsers from './js/api/users/apiUsers';
+// import productCard from './js/adv/productCard';
 
+// // import userData from './js/userData';
+// import {
+//   getPofileTest,
+//   favouritesFormMarkup
+// } from './js/profile/profileMarkups';
+
+// import {
+//   setCartCounter,
+//   setupEvents
+// } from './js/components/cart/cart';
+
+// import {
+//   addNewAndLastSeen
+// } from './js/components/new';
+// import userData from './js/userData';
+
+// const btnShowModal = document.getElementById('btnShowModal');
+
+// let currentItem = null;
+
+// btnShowModal.addEventListener('click', async () => {
+//   const response = await axios.get('https://goit-store.herokuapp.com/products%27');
+//   const item = response.data[8];
+//   currentItem = item;
+//   productCard(item);
+// });
+
+// export const initialAction = async () => {
+//   await apiProducts.getCategories();
+//   setting.getDevice(document.documentElement.clientWidth);
+
+//   containerHandler();
+//   addNewAndLastSeen();
+//   setCartCounter();
+//   setupEvents();
+//   new Sim();
+//   // trigger.triggerFn();
+//   // console.log('userData :>> ', userData);
+// };
+// initialAction();
+// // console.log('userdData :>> ', userData.user);
+
+// // apiAuth.login({
+// //   "email": "olysik@gmail.com",
+// //   "password": "olysik34"
+// // })
+// console.log('userdData :>> ', userData.user);
+// //=======================
 import './sass/main.scss';
 import './js/category/category-markup';
 import {
@@ -115,51 +191,29 @@ import axios from 'axios';
 import apiAuth from './js/api/auth/apiAuth';
 import apiUsers from './js/api/users/apiUsers';
 import productCard from './js/adv/productCard';
-
-// import userData from './js/userData';
+import userData from './js/userData';
 import {
   getPofileTest,
   favouritesFormMarkup
 } from './js/profile/profileMarkups';
-
-import {
-  setCartCounter,
-  setupEvents
-} from './js/components/cart/cart';
-
-import {
-  addNewAndLastSeen
-} from './js/components/new';
-import userData from './js/userData';
-
 const btnShowModal = document.getElementById('btnShowModal');
-
 let currentItem = null;
-
 btnShowModal.addEventListener('click', async () => {
-  const response = await axios.get('https://goit-store.herokuapp.com/products%27');
+  const response = await axios.get('https://goit-store.herokuapp.com/products');
   const item = response.data[8];
   currentItem = item;
   productCard(item);
 });
-
 export const initialAction = async () => {
   await apiProducts.getCategories();
   setting.getDevice(document.documentElement.clientWidth);
-
   containerHandler();
-  addNewAndLastSeen();
-  setCartCounter();
-  setupEvents();
   new Sim();
   // trigger.triggerFn();
-  // console.log('userData :>> ', userData);
+  console.log('userData :>> ', userData);
 };
 initialAction();
-// console.log('userdData :>> ', userData.user);
-
-// apiAuth.login({
-//   "email": "olysik@gmail.com",
-//   "password": "olysik34"
-// })
-console.log('userdData :>> ', userData.user);
+apiAuth.login({
+  "email": "olysik@gmail.com",
+  "password": "olysik34"
+})
--- src/js/sale/saleSection.js
+++ src/js/sale/saleSection.js
@@ -1,11 +1,85 @@
+// import vector_love from '../../images/sale/Vector_love.svg';
+// import vector from '../../images/sale/Vector.svg';
+// import { createSingleCardMarkup } from './cardModule';
+// import apiUsers from '../api/users/apiUsers';
+
+// // /* <section class="card container"></section> *
+
+// const createListMarkup = array => {
+//   return `
+//    <section class="card">
+//   <h2 class="card_description">Акции</h2>
+//   <ul class="card_list">${array.reduce((acc, element) => {
+//     acc += createSingleCardMarkup(element, 'sale');
+//     return acc;
+//   }, '')}</ul></section>`;
+// };
+// const favoritesArr = [];
+// const getItem = event => {
+//   if (event.target.closest('[data-id]') && event.target.nodeName === 'IMG') {
+//     const id = event.target.closest('[data-id]').dataset.id;
+//     console.log('id', id);
+//     if (event.target.src === vector) {
+//       event.target.src = vector_love;
+//       favoritesArr.push(id);
+//       apiUsers.addFavorite(id);
+
+//       localStorage.setItem('favorites', JSON.stringify(favoritesArr));
+//     } else if (event.target.src === vector_love) {
+//       event.target.src = vector;
+//       apiUsers.deleteFavorite(id);
+//       console.log(favoritesArr);
+//       const itemFavoriteID = favoritesArr.find(element => {
+//         element === id;
+
+//         console.log(id);
+//       });
+//       //   for (let arr of favoritesArr) {
+//       //     if (arr !== id) {
+//       //       favoritesArr = [];
+//       //       favoritesArr.push(arr);
+//       //     }
+//       //   }
+//       console.log('itemFavoriteID', itemFavoriteID);
+//     }
+
+//     // productCard(id);
+//     return id; // функция Ани(id)
+//   } else return;
+// };
+
+// ///
+
+// // apiUsers.getCurrentUser();
+// // apiUsers.getCurrentUser().then(data => console.log(data));
+
+// // const cardList = document.querySelector('.card_list');
+// export const createList = array => {
+//   const container = document.querySelector('.sections');
+//   container.innerHTML = createListMarkup(array);
+//   container.addEventListener('click', getItem);
+//   // cardList.addEventListener('click', getVector);
+
+//   // cardList.addEventListener('click', getVector);
+// };
+
+// // export const getVector = event => {
+// //   if (event.target.closest('[data-clickVector]') && event.target.nodeName === 'IMG') {
+// //     const clickVector = event.target.closest('[data-clickVector]').dataset.clickVector;
+// //     console.log('clickVector', clickVector);
+// //     return clickVector;
+// //   } else return;
+// // };
 import vector_love from '../../images/sale/Vector_love.svg';
 import vector from '../../images/sale/Vector.svg';
-import { createSingleCardMarkup } from './cardModule';
+import {
+  createSingleCardMarkup
+} from './cardModule';
 import apiUsers from '../api/users/apiUsers';
-
+import userData from '../userData'
 // /* <section class="card container"></section> *
-
 const createListMarkup = array => {
+  console.log(array)
   return `
    <section class="card">
   <h2 class="card_description">Акции</h2>
@@ -14,59 +88,39 @@ const createListMarkup = array => {
     return acc;
   }, '')}</ul></section>`;
 };
-const favoritesArr = [];
 const getItem = event => {
   if (event.target.closest('[data-id]') && event.target.nodeName === 'IMG') {
     const id = event.target.closest('[data-id]').dataset.id;
-    console.log('id', id);
     if (event.target.src === vector) {
       event.target.src = vector_love;
-      favoritesArr.push(id);
-      apiUsers.addFavorite(id);
-
-      localStorage.setItem('favorites', JSON.stringify(favoritesArr));
+      if (localStorage.getItem('info')) {
+        const token = localStorage.getItem('info');
+        const tokenParse = JSON.parse(token).token
+        if (tokenParse) {
+          apiUsers.addFavorite(id).then((data) => {
+            apiUsers.getCurrentUser().then(response => {
+              userData.user.favorites = response.data.favorites
+              console.log('userData.user', userData.user)
+            })
+          });
+        } else {
+          localStorage.setItem('favorites', JSON.stringify(favoritesArr));
+        }
+      }
     } else if (event.target.src === vector_love) {
       event.target.src = vector;
-      apiUsers.deleteFavorite(id);
-      console.log(favoritesArr);
-      const itemFavoriteID = favoritesArr.find(element => {
-        element === id;
-
-        console.log(id);
+      apiUsers.deleteFavorite(id)
+      favoritesArr.find(elem => {
+        elem !== id;
+        favoritesArr.push(element)
       });
-      //   for (let arr of favoritesArr) {
-      //     if (arr !== id) {
-      //       favoritesArr = [];
-      //       favoritesArr.push(arr);
-      //     }
-      //   }
-      console.log('itemFavoriteID', itemFavoriteID);
     }
-
     // productCard(id);
     return id; // функция Ани(id)
   } else return;
 };
-
-///
-
-// apiUsers.getCurrentUser();
-// apiUsers.getCurrentUser().then(data => console.log(data));
-
-// const cardList = document.querySelector('.card_list');
 export const createList = array => {
-  const container = document.querySelector('.sections');
+  const container = document.querySelector('.container');
   container.innerHTML = createListMarkup(array);
   container.addEventListener('click', getItem);
-  // cardList.addEventListener('click', getVector);
-
-  // cardList.addEventListener('click', getVector);
 };
-
-// export const getVector = event => {
-//   if (event.target.closest('[data-clickVector]') && event.target.nodeName === 'IMG') {
-//     const clickVector = event.target.closest('[data-clickVector]').dataset.clickVector;
-//     console.log('clickVector', clickVector);
-//     return clickVector;
-//   } else return;
-// };
