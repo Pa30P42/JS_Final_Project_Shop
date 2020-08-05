92bd2295cba50edda536dc995e37856446b725b4
nyuta <pillowforneedles@gmail.com>
2020-08-06 02:14:25 +0300

    adv favorites

--- src/js/adv/favorite.js
+++ src/js/adv/favorite.js
@@ -1,9 +1,64 @@
-// import apiUsers from '../api/users/apiUsers';
-// import userData from '../../js/userData';
-// import favoriteFill from '../../images/adv/icons/favorite_fill.svg';
-// import favorite from '../../images/adv/icons/favorite.svg';
-
+const INFO = 'info';
 // const FAVORITES = 'favorites';
+
+import apiUsers from '../api/users/apiUsers';
+import userData from '../../js/userData';
+import favorite from '../../images/adv/icons/favorite.svg';
+import favoriteFill from '../../images/adv/icons/favorite_fill.svg';
+
+function updateFavorites(id) {
+  const refBtnFavourite = document.getElementById('btnFavourite');
+  const refChangeTextFavorite = document.querySelector('.adv__in-favorite');
+
+  const favoriteData = localStorage.getItem(INFO); //array
+  const info = JSON.parse(favoriteData);
+  let favorites = info.favorites;
+  // if (!favorites) {
+  // favorites = [];
+  // refBtnFavourite.src = favorite;
+  // refChangeTextFavorite.textContent = 'В избранное';
+  // }
+  // const item = favorites.find(elem => elem.id === id);
+  console.log(favorites);
+  if (favorites.find(elem => elem === id)) {
+    refBtnFavourite.src = favoriteFill;
+    refChangeTextFavorite.textContent = 'Из избранного';
+  }
+
+  refBtnFavourite.addEventListener('click', async event => {
+    // const favoriteData = localStorage.getItem(INFO);
+    // let favorites = JSON.parse(favoriteData);
+
+    if (event.target.src === favorite) {
+      event.target.src = favoriteFill;
+      refChangeTextFavorite.textContent = 'Из избранного';
+
+      favorites.push(id);
+      localStorage.setItem(INFO, JSON.stringify(favorites));
+
+      await apiUsers.addFavorite(id);
+      // userData.favorite = [...userData.favorite, item];
+      // console.log(userData.favorite);
+    } else if (event.target.src === favoriteFill) {
+      event.target.src = favorite;
+      refChangeTextFavorite.textContent = 'В избранное';
+
+      const indexFavorites = favorites.indexOf(id);
+      favorites.splice(indexFavorites, 1);
+      localStorage.setItem(INFO, JSON.stringify(favorites));
+
+      await apiUsers.deleteFavorite(id);
+    }
+    // --------- проверка ---/
+    // console.log('id ', id._id);
+    // apiUsers.addFavorite(id._id).then(data => console.log('favorites ', data));
+    // apiUsers.getCurrentUser().then(data => console.log('current ', data));
+    // ----------------------/
+    // }
+  });
+}
+
+export default updateFavorites;
 
 // const addToFavourite = async id => {
 //   await apiUsers.addFavorite(id);
@@ -12,19 +67,19 @@
 //   apiUsers.addFavorite(`${id}`).then(data => console.log('favorites ', data));
 //   apiUsers.getCurrentUser().then(data => console.log('current ', data));
 
-//   // userData.favorite = [...userData.favorite, id];
-//   // console.log(userData.favorite);
+// userData.favorite = [...userData.favorite, id];
+// console.log(userData.favorite);
 
-//   // const favoriteToLocalStorage = localStorage.getItem(FAVORITES);
-//   // let favorites = JSON.parse(favoriteToLocalStorage);
-//   // if (!favorites) {
-//   //   favorites = [];
-//   // }
-//   // const item = favorites.find(elem => elem.id === id);
-//   // if (!item) {
-//   //   favorites.push(id);
-//   // }
-//   // localStorage.setItem(FAVORITES, JSON.stringify(favorites));
+// const favoriteData = localStorage.getItem(FAVORITES);
+// let favorites = JSON.parse(favoriteData);
+// if (!favorites) {
+//   favorites = [];
+// }
+// const item = favorites.find(elem => elem.id === id);
+// if (!item) {
+//   favorites.push(id);
+// }
+// localStorage.setItem(FAVORITES, JSON.stringify(favorites));
 // };
 
 // const removeFavorite = async id => {
--- src/js/adv/productCard.js
+++ src/js/adv/productCard.js
@@ -1,7 +1,7 @@
 import { modalModule } from '../components/modalModule/modalModule';
 import markup from './markup';
 import updateLastSeen from './lastSeen';
-import addToFavourite from './favorite';
+import updateFavorites from './favorite';
 import buy from './buy';
 import favoriteFill from '../../images/adv/icons/favorite_fill.svg';
 import favorite from '../../images/adv/icons/favorite.svg';
@@ -14,49 +14,64 @@ const productCard = async item => {
   // const item = items.find(i => i._id === id);
   // console.log('items ', items);
   // console.log('item ', item);
-  // console.log('id ', id);
-
+  // console.log('id ', item._id);
   const component = () => markup(item);
   // console.log(item);
   updateLastSeen(item._id);
   modalModule(component, addListener);
+  updateFavorites(item._id);
 
   const btnBuy = document.getElementById('btnBuy');
-  const btnFavourite = document.getElementById('btnFavourite');
-  const changeTextFavorite = document.querySelector('.adv__in-favorite');
-  const FAVORITES = 'favorites';
+  // ---------- favorites ---------
+  // const btnFavourite = document.getElementById('btnFavourite');
+  // const changeTextFavorite = document.querySelector('.adv__in-favorite');
+  // const FAVORITES = 'favorites';
 
-  btnFavourite.addEventListener('click', async event => {
-    const favoriteToLocalStorage = localStorage.getItem(FAVORITES);
-    let favorites = JSON.parse(favoriteToLocalStorage);
+  // const favoriteToLocalStorage = localStorage.getItem(FAVORITES); //array
+  // let favorites = JSON.parse(favoriteToLocalStorage);
 
-    if (event.target.src === favorite) {
-      event.target.src = favoriteFill;
-      changeTextFavorite.textContent = 'Из избранного';
+  // // if (!favorites) {
+  // //   // favorites = [];
+  // //   btnFavourite.src = favorite;
+  // //   changeTextFavorite.textContent = 'В избранное';
+  // // }
 
-      favorites.push(item);
-      localStorage.setItem(FAVORITES, JSON.stringify(favorites));
+  // if (favorites.find(elem => elem._id === item._id)) {
+  //   btnFavourite.src = favoriteFill;
+  //   changeTextFavorite.textContent = 'Из избранного';
+  // }
 
-      await apiUsers.addFavorite(item);
-      // userData.favorite = [...userData.favorite, item];
-      // console.log(userData.favorite);
-    } else if (event.target.src === favoriteFill) {
-      event.target.src = favorite;
-      changeTextFavorite.textContent = 'В избранное';
+  // btnFavourite.addEventListener('click', async event => {
+  //   const favoriteToLocalStorage = localStorage.getItem(FAVORITES);
+  //   let favorites = JSON.parse(favoriteToLocalStorage);
 
-      const indexFavorites = favorites.indexOf(item);
-      favorites.splice(indexFavorites, 1);
-      localStorage.setItem(FAVORITES, JSON.stringify(favorites));
+  //   if (event.target.src === favorite) {
+  //     event.target.src = favoriteFill;
+  //     changeTextFavorite.textContent = 'Из избранного';
 
-      await apiUsers.deleteFavorite(item);
-    }
-    // --------- проверка ---/
-    // console.log('id ', id._id);
-    // apiUsers.addFavorite(id._id).then(data => console.log('favorites ', data));
-    // apiUsers.getCurrentUser().then(data => console.log('current ', data));
-    // ----------------------/
-    // }
-  });
+  //     favorites.push(item);
+  //     localStorage.setItem(FAVORITES, JSON.stringify(favorites));
+
+  //     await apiUsers.addFavorite(item._id);
+  //     // userData.user.favorite = [...userData.favorite, item];
+  //     // console.log(userData.user.favorite);
+  //   } else if (event.target.src === favoriteFill) {
+  //     event.target.src = favorite;
+  //     changeTextFavorite.textContent = 'В избранное';
+
+  //     const indexFavorites = favorites.indexOf(item._id);
+  //     favorites.splice(indexFavorites, 1);
+  //     localStorage.setItem(FAVORITES, JSON.stringify(favorites));
+
+  //     await apiUsers.deleteFavorite(item._id);
+  //   }
+  //   // --------- проверка ---/
+  //   // console.log('id ', id._id);
+  //   // apiUsers.addFavorite(id._id).then(data => console.log('favorites ', data));
+  //   // apiUsers.getCurrentUser().then(data => console.log('current ', data));
+  //   // ----------------------/
+  //   // }
+  // });
 
   function addListener(closeModal) {
     const btnBuy = document.getElementById('btnBuy');
