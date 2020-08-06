b139c6a76c7c409aac576ddfb81f828a2d7436fd
OlegKhodakivskyi <oleghodakovsky@gmail.com>
2020-08-06 23:50:09 +0300

    before merge backfeedback Ismail

--- src/js/auth/authForm.js
+++ src/js/auth/authForm.js
@@ -1,9 +1,12 @@
 import close from '../../images/information/close-icon.svg';
 
-const user = {
-  email: '${}',
-  password: '',
+const authForms = {
+  authForm: {
+    email: '',
+    password: '',
+  },
 };
+
 
 export const authForm = () => {
   return `
@@ -17,7 +20,7 @@ export const authForm = () => {
     <input 
     type="email"
     name="email"
-    class="authFormEmail" required placeholder="Email или телефон"/>
+    class="authFormEmail" placeholder="Email или телефон"/>
 
   <label for="password">
     <em class="authorization-star">*</em>
@@ -27,7 +30,7 @@ export const authForm = () => {
     <input
     type="password"
     name="password"
-    class="authFormPassword" required placeholder="Пароль"/>
+    class="authFormPassword" placeholder="Пароль"/>
 
     <button 
     type="submit" 
@@ -53,3 +56,45 @@ export const authForm = () => {
   </form>
     `;
 };
+
+
+// const authSignInBtn = document.querySelector('.btn__sign_in');
+// authSignInBtn.addEventListener('click', event => {
+//   apiUsers
+//     .changeUserInfo({
+//       email: forms.authForm.email,
+//       password: forms.authForm.password,
+//     })
+//     .then(() => {
+//       userData.user = {
+//         ...userData.user,
+//         ...forms.authForm,
+//       };
+//     });
+// });
+
+
+function validateForm(event) {
+
+  const authField = event.target;
+  const authInputValue = event.target.value;
+  const authInputLength = event.target.value.length;
+  const authNameOfInput = authField.getAttribute('name');
+  const passwordRedEx = /(?=.*[a-zA-Zа-яёА-Я])/;
+  const regExEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{3,}))$/;
+
+  if (authNameOfInput === 'email') {
+    authNameOfInput === 'email' && authInputValue.match(regExEmail)
+      ? ((authField.nextElementSibling.innerHTML = `<span class="helper-text-valid"></span>`),
+        (authField.style.outlineColor = '#109b17'))
+      : ((authField.nextElementSibling.innerHTML = `<span class="helper-text-invalid">Неверный адрес почты</span>`),
+        (authField.style.outlineColor = '#FF8A9D'));
+  }  else if (authNameOfInput === 'password') {
+    authNameOfInput === 'password' && authInputLength > 7 && authInputValue.match(passwordRedEx)
+      ? ((authField.nextElementSibling.innerHTML = `<span class="helper-text-valid"></span>`),
+        (authField.style.outlineColor = '#109b17'))
+      : ((authField.nextElementSibling.innerHTML = `<span class="helper-text-invalid"><small>Пароль должен содержать не менее 8 символов</small></span>`),
+        (authField.style.outlineColor = '#FF8A9D'));
+  }
+}
+
